a79ddc17ae393547d2564ce488b989c7b6163ade
Simon <01secn@gmail.com>
2019-04-11 11:40:55 +0100

    Added material ui library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Button variant="contained" color="secondary">Click me!</Button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
